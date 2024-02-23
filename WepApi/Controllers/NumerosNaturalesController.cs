5a08023a76761e5f325b4c0b4b6c7acdf603ccae
Luis Angel Ramírez Galicia <l.angel.ramirez.g1996@gmail.com>
2024-02-23 01:14:35 -0600

    Add project files.

---
+++ WepApi/Controllers/NumerosNaturalesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WepApi.Controllers
+{
+    public class NumerosNaturalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("extract/{entrada}")]
+        public IActionResult Extract(string entrada)
+        {   //string entrada = string.Empty;
+            int numero = 0;
+            string exSalida = "";
+
+            try 
+            { 
+                numero = int.Parse(entrada); 
+            }
+            catch (Exception ex) 
+            {
+                numero = 101;
+                exSalida = "la entrada no es un numero ";
+            }
+            
+
+            NumerosNaturales numerosNaturales = new NumerosNaturales();
+            numerosNaturales.Numeros = new List<int> { };
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                numerosNaturales.Numeros.Add(i);
+            }
+
+            if (numero >= 0 && numero < 100)
+            {
+            numerosNaturales.Extract(numero);
+                HttpContext.Session.SetString("Objeto", JsonSerializer.Serialize(numerosNaturales));
+                return Ok("El numero se extrajo correctamente");
+            }
+            else
+            {
+                exSalida = exSalida + "tiene que estar entre 0 y 99";
+                return BadRequest(exSalida);
+            }
+           
+        }
+
+
+        [HttpGet("cualExtrajo")]
+        public IActionResult RecuperarExtraido()
+        {   //string entrada = string.Empty;
+
+            string temp = HttpContext.Session.GetString("Objeto");
+            //result = (ML.Result)temp; 
+            NumerosNaturales result = JsonSerializer.Deserialize<NumerosNaturales>(temp)!;
+
+            int retorno = result.CualSeExtrajo();
+
+            return Ok(retorno);
+        }
+
+
+
+
+
+    }
+}
---
+++ WepApi/NumerosNaturales.cs
@@ -0,0 +1,33 @@
+﻿namespace WepApi
+{
+    public class NumerosNaturales
+    {
+        public List<int>? Numeros {  get; set; }
+       
+        public void Extract (int numeroExtraer)
+        {
+            Numeros.RemoveAt(numeroExtraer);
+
+        }
+
+        public int CualSeExtrajo()
+        {  
+            
+            int extraido = 0;
+            int temporal = 0;
+            foreach(int i in Numeros)
+            {
+                temporal = temporal + i;
+            }
+            //4950 es los primeros 100 numeros sumados del 0 al 99
+
+            extraido = 4950 - temporal;
+            return extraido;
+        }
+
+
+
+
+
+    }
+}
